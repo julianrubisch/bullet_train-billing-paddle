10bce8cc72f97b6810e3bfe93296cb342436873e
Julian Rubisch <julian@julianrubisch.at>
2023-07-12 09:47:41 +0200

    chore: Redirect to refresh URL after successful checkout

--- app/controllers/account/billing/paddle/subscriptions_controller.rb
+++ app/controllers/account/billing/paddle/subscriptions_controller.rb
@@ -7,4 +7,10 @@ class Account::Billing::Paddle::SubscriptionsController < Account::ApplicationCo
   # GET/POST /account/billing/paddle/subscriptions/:id/checkout.json
   def checkout
   end
+
+  def refresh
+    # could be done akin to Stripe with https://developer.paddle.com/api-reference/89c1805d821c2-list-transactions, but we have no access to the checkout_id here
+    # so we're just redirecting with a notice
+    redirect_to [:account, @subscription.generic_subscription.team], notice: t("billing/paddle/subscriptions.notifications.refreshed")
+  end
 end
