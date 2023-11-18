b2dfa85f836a5d34f230f4a2b178fc05f7e2e995
Julian Rubisch <julian@julianrubisch.at>
2023-11-18 20:30:03 +0100

    chore: Use ability_support hook to insert subscription abilities

--- lib/bullet_train/billing/paddle.rb
+++ lib/bullet_train/billing/paddle.rb
@@ -5,7 +5,14 @@ require "paddle"
 module BulletTrain
   module Billing
     module Paddle
-      # Your code goes here...
+      module AbilitySupport
+        def apply_billing_abilities(user)
+          super
+          can :read, ::Billing::Paddle::Subscription, team_id: user.team_ids
+          can :manage, ::Billing::Paddle::Subscription, team_id: user.administrating_team_ids
+          can [:update], ::Billing::Subscription, team_id: user.administrating_team_ids
+        end
+      end
     end
   end
 end
@@ -13,3 +20,5 @@ end
 def paddle_billing_enabled?
   ENV["PADDLE_API_KEY"].present?
 end
+
+ActiveSupport.on_load(:bullet_train_billing_ability_support) { prepend BulletTrain::Billing::Paddle::AbilitySupport }
