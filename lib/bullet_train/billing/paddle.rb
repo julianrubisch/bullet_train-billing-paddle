a76b2aa0e53cca803e14e98a1aa7b31fc1b62e75
Julian Rubisch <julian@julianrubisch.at>
2023-11-18 20:44:57 +0100

    chore: Use Teams::Base load hook to insert paddle sub association

--- lib/bullet_train/billing/paddle.rb
+++ lib/bullet_train/billing/paddle.rb
@@ -5,6 +5,16 @@ require "paddle"
 module BulletTrain
   module Billing
     module Paddle
+      module Teams
+        module Base
+          extend ActiveSupport::Concern
+
+          included do
+            has_many :billing_paddle_subscriptions, class_name: "Billing::Paddle::Subscription", dependent: :destroy, foreign_key: :team_id
+          end
+        end
+      end
+
       module AbilitySupport
         def apply_billing_abilities(user)
           super
@@ -21,4 +31,5 @@ def paddle_billing_enabled?
   ENV["PADDLE_API_KEY"].present?
 end
 
+ActiveSupport.on_load(:bullet_train_teams_base) { include BulletTrain::Billing::Paddle::Teams::Base }
 ActiveSupport.on_load(:bullet_train_billing_ability_support) { prepend BulletTrain::Billing::Paddle::AbilitySupport }
